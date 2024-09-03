d71d40baab64fbae3492e594bc970d68eff29b42
israel <israelwendellc@gmail.com>
2024-09-03 18:56:25 -0300

    change the example

--- src/Components/Title/index.js
+++ src/Components/Title/index.js
@@ -3,7 +3,7 @@ import "./Title.css"
 function Title() {
     return (
       <div>
-        <h1 className="Title">Task TO DO!!</h1>
+        <h1 className="Title">Task</h1>
       </div>  
     )
   }
